db33158cb15cca825ca37f637c8d56f39a2947d0
Siam Hossain <mdsiamh77@gmail.com>
2026-03-08 23:00:00 +0600

    update user views

--- users/views.py
+++ users/views.py
@@ -1,21 +1,21 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from yaml import serializer
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+
 from users.models import User
 from users.serializers import UserProfileSerializer
-from rest_framework import request, status
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework.parsers import MultiPartParser, FormParser
 
 
 class UserProfileView(ModelViewSet):
+
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
     parser_classes = [MultiPartParser, FormParser]
 
     def get_queryset(self):
-        # User can only access his own profile
         return User.objects.filter(id=self.request.user.id)
 
     @swagger_auto_schema(
@@ -24,22 +24,21 @@ class UserProfileView(ModelViewSet):
         responses={200: UserProfileSerializer}
     )
     def list(self, request, *args, **kwargs):
-        """Return profile data of authenticated user"""
+
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
 
     @swagger_auto_schema(
         operation_summary="Update user profile",
-        operation_description="Authenticated user can update their profile details like phone, location etc.",
         request_body=UserProfileSerializer,
-        responses={
-            200: UserProfileSerializer,
-            400: "Bad Request"
-        }
     )
     def update(self, request, *args, **kwargs):
-        user = request.user
-        serializer = self.get_serializer(user, data=request.data, partial=True)
+
+        serializer = self.get_serializer(
+            request.user,
+            data=request.data,
+            partial=True
+        )
 
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -47,30 +46,29 @@ class UserProfileView(ModelViewSet):
         return Response(serializer.data)
 
     @swagger_auto_schema(
-        operation_summary="Partial update user profile",
-        operation_description="Partially update profile fields.",
+        operation_summary="Partial update profile",
         request_body=UserProfileSerializer,
-        responses={200: UserProfileSerializer}
     )
     def partial_update(self, request, *args, **kwargs):
-        """Partial update profile"""
-        return super().partial_update(request, *args, **kwargs)
 
-    @swagger_auto_schema(
-        auto_schema=None
-    )
+        serializer = self.get_serializer(
+            request.user,
+            data=request.data,
+            partial=True
+        )
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
     def create(self, request, *args, **kwargs):
-        """Disable POST method"""
         return Response(
             {"detail": "Method 'POST' not allowed."},
             status=status.HTTP_405_METHOD_NOT_ALLOWED
         )
 
-    @swagger_auto_schema(
-        auto_schema=None
-    )
     def destroy(self, request, *args, **kwargs):
-        """Disable DELETE method"""
         return Response(
             {"detail": "Method 'DELETE' not allowed."},
             status=status.HTTP_405_METHOD_NOT_ALLOWED
